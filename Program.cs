7989f73314dcc85051678096cf7f7b5f7da1535e
KraSS <groverkras@gmail.com>
2024-07-16 14:45:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+namespace MagniseFinAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddHttpClient();
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
